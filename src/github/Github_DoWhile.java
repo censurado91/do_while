94ff3147239a9050573e993722ceaf3886f83512
censurado91 <tahtikytkenta@gmail.com>
2020-03-15 00:47:22 +0100

    ejemplo de un do while comentado en 3 idiomas

---
+++ src/github/Github_DoWhile.java
@@ -0,0 +1,34 @@
+package github;
+
+public class Github_DoWhile {
+
+	public static void main(String[] args) {
+		/*
+		 * la variable se inicializar· a cero.
+		 * the variable will start being zero.
+		 * muuttaja alkaa nollalla
+		 */
+		int i=0;
+		
+		do {
+			/*
+			 * esto mostrar· el valor de i
+			 * this will show the value of i
+			 * t‰m‰ n‰yttaa paljon i arvostaa.
+			 */
+			System.out.println(i);
+			/*
+			 * incremento.
+			 * increment.
+			 * lis‰‰ntyminen.
+			 */
+			i++;
+			/*
+			 * lo repetir· hasta que i=10
+			 * it will be repeated until i=10
+			 * se toistaa kunnes i=10.
+			 */
+		}while(i<10);
+	}
+
+}
